7f2626f1b4817db6da55e607310bd142b256d633
Jennifer Hott-Leitsch <jenniferhott-leitsch@Mac-mini.hsd1.il.comcast.net>
2020-10-14 19:05:39 -0500

    updated styling to match closer to Disney brand

--- src/components/MessageBoardList.js
+++ src/components/MessageBoardList.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 //import components
 import MessageBoard from "./MessageBoard";
+import Links from "../components/Links";
 
 function MessageBoardList() {
     const [messages, setMessages] = useState([]);
@@ -22,6 +23,13 @@ function MessageBoardList() {
 
     return(
         <div>
+        <header>
+            <nav>
+                <div className='nav-links'>
+                    <Links />
+                </div>
+            </nav>
+      </header>
             <section className="messageList grid-view">
                 {messages.map((data, index) => {
                     return <MessageBoard parentId={data.parent_id} title={data.title} children={data.children} attraction={data.attraction} time={data.time} key={index} />
